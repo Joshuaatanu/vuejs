abb5801c8400fd7ec91bb586a2497e4d22cae527
Joshua Atanu <atanu.joshua@gmail.com>
2024-08-09 14:44:20 +0100

    created a vue app

---
+++ vue_2/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'The Final Empire',
+            author: 'joshua Atanu',
+            age : '25'
+        }
+  }
+});
+
+app.mount('#app')
