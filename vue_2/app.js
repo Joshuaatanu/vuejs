6bd18299132d34d013e359dbf40c8176b6af7534
Joshua Atanu <atanu.joshua@gmail.com>
2024-08-09 15:48:16 +0100

    conditional rendering

--- vue_2/app.js
+++ vue_2/app.js
@@ -1,11 +1,29 @@
 const app = Vue.createApp({
     data() {
         return {
+            showBooks: true,
             title: 'The Final Empire',
             author: 'joshua Atanu',
             age : '25'
         }
-  }
+    },
+    methods: {
+        // changeTitle(title) {
+        //     this.title = title
+        // },
+        // increaseNumber() {
+        //     this.age++;
+        // },
+        // decreaseNumber() {
+        //     this.age--
+        // }
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        }
+    }
+    
+
+
 });
 
 app.mount('#app')
