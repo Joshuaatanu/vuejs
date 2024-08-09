0faee849fae858716d81fc8d6ed1086bd0e750a3
Joshua Atanu <atanu.joshua@gmail.com>
2024-08-09 16:06:05 +0100

    mouse event

--- vue_2/app.js
+++ vue_2/app.js
@@ -4,7 +4,9 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'The Final Empire',
             author: 'joshua Atanu',
-            age : '25'
+            age: '25',
+            x: 0,
+            y: 0
         }
     },
     methods: {
@@ -17,9 +19,19 @@ const app = Vue.createApp({
         // decreaseNumber() {
         //     this.age--
         // }
-        toggleShowBooks() {
-            this.showBooks = !this.showBooks
-        }
+        // toggleShowBooks() {
+        //     this.showBooks = !this.showBooks
+        // },
+        // handleEvent(e, data) {
+        //     console.log(e, e.type)
+        //     if (data) {
+        //         console.log(data)
+        //     }
+        // },
+        // handleMouseMove(e) {
+        //     this.x = e.offsetX
+        //     this.y = e.offsetY
+        // }
     }
     
 
